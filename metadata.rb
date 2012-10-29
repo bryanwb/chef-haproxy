8045cf27554b6e4a4f2571c43e0f6051d81afaaf
Bryan Berry <bryan.berry@gmail.com>
2012-10-29 13:00:04 +0100

    bump version #

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
+name "haproxy"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures haproxy 1.5dev"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.5"
+version           "0.0.7"
 
 recipe "haproxy", "Installs and configures haproxy 1.5dev"
-recipe "haproxy::app_lb", "Installs and configures haproxy by searching for nodes of a particular role"
 
 %w{ debian ubuntu redhat centos fedora}.each do |os|
   supports os
